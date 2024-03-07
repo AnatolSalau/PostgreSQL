c60b355ea1286a4d5651c739eb0b39b1d7f4011e
AnatolSalau <salovanatoly5@mail.ru>
2024-03-07 15:13:40 +0300

    Получение диапазона строк. LIMIT и OFFSET

--- src/main/resources/metanit/_3_selects/_4_filter_in.sql
+++ src/main/resources/metanit/_3_selects/_4_filter_in.sql
@@ -14,16 +14,4 @@ VALUES
       ('Galaxy S8 Plus', 'Samsung', 2, 46000),
       ('Desire 12', 'HTC', 3, 26000);
 
-/* Оператор LIMIT позволяет извлечь определенное количество строк: */
-SELECT * FROM Products
-ORDER BY ProductName
-LIMIT 4;
-
-/* Оператор OFFSET позволяет указать, с какой строки надо начинать выборку.
-   Например, выберем 3 строки, начиная со 2-й:*/
-
-SELECT * FROM Products
-ORDER BY ProductName
-LIMIT 3 OFFSET 2;
-
 
