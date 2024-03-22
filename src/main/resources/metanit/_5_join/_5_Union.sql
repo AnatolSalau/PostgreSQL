d9878c3aef3118cea087f19e0d2f26a657df0adf
AnatolSalau <salovanatoly5@mail.ru>
2024-03-22 11:59:19 +0300

    UNIONS

--- src/main/resources/metanit/_5_join/_5_Union.sql
+++ src/main/resources/metanit/_5_join/_5_Union.sql
@@ -35,3 +35,38 @@ SELECT FirstName, LastName
 FROM Customers
 UNION ALL SELECT FirstName, LastName
 FROM Employees;
+
+SELECT FirstName, LastName
+FROM Customers
+UNION SELECT FirstName, LastName FROM Employees;
+
+-- объединение с дубликатами
+SELECT FirstName, LastName
+FROM Customers
+UNION ALL SELECT FirstName, LastName FROM Employees;
+
+-- склеим данные с колонок FirstName и LastName в одну
+SELECT FirstName || ' ' || LastName AS FullName
+FROM Customers;
+
+--обьединение со склейкой данных
+
+SELECT customers.firstname || ' ' || customers.lastname
+FROM Customers
+UNION SELECT employees.firstname || ' ' || employees.lastname
+FROM Employees;
+
+/*
+      Сделать выборку из таблицы Customers
+      вывести имя и фамилию и начислить проценты
+            если сумма меньше 2800 -> начислить 50 процентов
+            если сумма больше 2800 -> начислить 10 процентов
+      Формула подсчета процентов:
+            10 процентов:num + num * 0.1
+            50 процентов:num + num * 0.5
+ */
+
+SELECT FirstName, LastName, AccountSum, AccountSum + AccountSum * 0.5 AS total_with_percent, (AccountSum + AccountSum * 0.5) - AccountSum AS percents
+FROM Customers WHERE Customers.AccountSum < 2800
+UNION ALL SELECT FirstName, LastName, AccountSum, AccountSum + AccountSum * 0.1 AS total_with_percent, (AccountSum + AccountSum * 0.1) - AccountSum AS percents
+FROM Customers WHERE Customers.AccountSum >= 2800
