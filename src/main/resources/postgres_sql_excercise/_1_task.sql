ea6b836b84731a0ff4cc3afbd2482ef53ad70ac8
AnatolSalau <salovanatoly5@mail.ru>
2024-04-03 12:04:38 +0300

    Combining results from multiple queries

--- src/main/resources/postgres_sql_excercise/_1_task.sql
+++ src/main/resources/postgres_sql_excercise/_1_task.sql
@@ -14,3 +14,40 @@ SELECT name,
              END
              AS cost
 FROM cd.facilities;
+
+/**
+Select month from timestamp
+ */
+SELECT EXTRACT(month FROM '2022-01-01 12:34:56'::timestamp) AS month;
+
+/**
+  SQL Query to Get the Month from a Date in PostgreSQL
+ */
+
+SELECT EXTRACT(month FROM date_column) AS month FROM table_name;
+
+/**
+  How can you produce a list of members who joined after the start of September 2012?
+  Return the memid, surname, firstname, and joindate of the members in question.
+ */
+SELECT memid, surname, firstname, joindate FROM cd.members
+WHERE
+      (EXTRACT(month FROM joindate) >= 9)
+  AND
+      (EXTRACT(year FROM joindate) >= 2012);
+
+/**
+  How can you produce an ordered list of the first 10 surnames in the members table?
+  The list must not contain duplicates.
+ */
+SELECT DISTINCT surname FROM cd.members
+ORDER BY surname
+LIMIT 10;
+
+/**
+  https://pgexercises.com/questions/basic/union.html
+  Combining results from multiple queries
+  Question
+  You, for some reason, want a combined list of all surnames and all facility names.
+  Yes, this is a contrived example :-). Produce that list!
+ */
