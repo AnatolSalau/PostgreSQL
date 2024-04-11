439ad3694d506ae776ba5abd3c79b8e10a76c6fb
AnatolSalau <salovanatoly5@mail.ru>
2024-04-11 12:26:32 +0300

    Postgres transaction

--- src/main/resources/postgres_sql_excercise/_2_join_subquery.sql
+++ src/main/resources/postgres_sql_excercise/_2_join_subquery.sql
@@ -6,3 +6,22 @@ SELECT starttime FROM cd.bookings
                             JOIN cd.members ON cd.bookings.memid = cd.members.memid
 WHERE cd.members.firstname = 'David'
   AND cd.members.surname = 'Farrell';
+
+/*
+ How can you output a list of all members who have recommended another member?
+ Ensure that there are no duplicates in the list, and that results are ordered by (surname, firstname).
+ */
+SELECT DISTINCT recommender.firstname, recommender.surname FROM cd.members as member
+          JOIN cd.members as recommender ON member.recommendedby = recommender.memid
+ORDER BY surname, firstname;
+
+/*
+ How can you output a list of all members, including the individual who recommended them (if any)?
+ Ensure that results are ordered by (surname, firstname).
+ */
+SELECT
+      member.firstname AS memfname, member.surname AS memsname,
+      recommender.firstname AS recfname, recommender.surname  recsname
+FROM cd.members  member
+           LEFT JOIN cd.members AS recommender ON member.recommendedby = recommender.memid
+ORDER BY memsname, memfname;
