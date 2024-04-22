b9beaf145f22debb332e46fda157263850a6f4cc
AnatolSalau <salovanatoly5@mail.ru>
2024-04-22 12:08:19 +0300

    Postgres exercises

--- src/main/resources/postgres_sql_excercise/_2_join_subquery.sql
+++ src/main/resources/postgres_sql_excercise/_2_join_subquery.sql
@@ -29,3 +29,27 @@ ORDER BY memsname, memfname;
 /*
  https://pgexercises.com/questions/joins/threejoin.html
  */
+select mems.firstname || ' ' || mems.surname as member,
+       facs.name as facility,
+       case
+             when mems.memid = 0 then
+                         bks.slots*facs.guestcost
+             else
+                         bks.slots*facs.membercost
+             end as cost
+from
+      cd.members mems
+            inner join cd.bookings bks
+                       on mems.memid = bks.memid
+            inner join cd.facilities facs
+                       on bks.facid = facs.facid
+where
+            date(bks.starttime) >= '2012-09-14' and
+            date(bks.starttime) < '2012-09-15' and (
+            (mems.memid = 0 and bks.slots*facs.guestcost > 30) or
+            (mems.memid != 0 and bks.slots*facs.membercost > 30)
+      )
+order by cost desc;
+/*
+ https://pgexercises.com/questions/joins/sub.html
+ */
