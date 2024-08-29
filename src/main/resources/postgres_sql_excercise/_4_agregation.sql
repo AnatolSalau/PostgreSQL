c689bc371347d42f05663dd4e43cb406078c54b9
AnatolSalau <salovanatoly5@mail.ru>
2024-08-29 13:12:52 +0300

    SQL task windows function

--- src/main/resources/postgres_sql_excercise/_4_agregation.sql
+++ src/main/resources/postgres_sql_excercise/_4_agregation.sql
@@ -4,4 +4,10 @@ FROM cd.bookings AS book
 WHERE
         EXTRACT(MONTH FROM book.starttime) = 9 AND EXTRACT(YEAR FROM book.starttime) = 2012
 GROUP BY book.facid
-ORDER BY "Total Slots"
+ORDER BY "Total Slots";
+
+--https://pgexercises.com/questions/aggregates/countmembers.html
+SELECT COUNT(memid) OVER(),
+       CASE WHEN memid = 0 THEN 'GUEST' ELSE firstname  END AS firstname,
+       CASE WHEN memid = 0 THEN 'GUEST' ELSE surname  END AS surname
+FROM cd.members
